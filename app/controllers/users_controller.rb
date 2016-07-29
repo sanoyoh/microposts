3500f4a5f6242b8e0c7920ff11e73558c10d2382
sanoyo <sa178kla@docomo.ne.jp>
2016-07-29 05:22:15 +0000

    add sign sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
     @user = User.new
   end 
   
-  def creat
+  def create
     @user = User.new(user_params)
     if @user.save
+        flash[:sucess]= "Welcome to the Sample App!"
         redirect_to @user 
     else
       render 'new'
