58b13408459a5e519ad41c5b0c839af3770fd56c
sanoyo <sa178kla@docomo.ne.jp>
2016-08-07 14:34:02 +0000

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     has_secure_password
+  validates :age, numericality: {
+            only_integer: true, greater_than_or_equal_to:0, less_than_or_equal_to: 100
+  }
 end
